eabbf1d258ae4279fb5b7f6cef377964c6990a56
Mihanikus55 <mihaelnovikov2@yandex.ru>
2023-11-20 18:00:13 +0300

    Third commit

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    print('My first git program')
+    print('And I change it everyday.')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
